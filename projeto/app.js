6e47b6d77813103fa06944609f1eeef7fdf45d48
Yuri-Alves <yuri.moreira.alves@gmail.com>
2022-03-07 21:31:56 -0300

    Tranferencia EJS

--- projeto/app.js
+++ projeto/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var homeRouter = require('./routes/home');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
